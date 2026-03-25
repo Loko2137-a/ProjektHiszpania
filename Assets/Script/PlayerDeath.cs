1e594e40124aa4ab28997712a5e95d1bca67843f
Loko2137-a <wojm07loko@gmail.com>
2026-03-25 23:31:51 +0100

    Added audio, and added animations

--- Assets/Script/PlayerDeath.cs
+++ Assets/Script/PlayerDeath.cs
@@ -6,17 +6,38 @@ public class PlayerDeath : MonoBehaviour
 {
     public Transform player;
     public GameObject deathPanel;
+    public GameObject endPanel;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("DeathPart"))
         {
-            player.GetComponent<ThirdPersonController>().MoveSpeed = 0;
-            player.GetComponent<ThirdPersonController>().SprintSpeed = 0;
+            ThirdPersonController controller = player.GetComponent<ThirdPersonController>();
+
+            controller.MoveSpeed = 0;
+            controller.SprintSpeed = 0;
+
             deathPanel.SetActive(true);
         }
     }
     public void restrtScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void nextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int totalScenes = SceneManager.sceneCountInBuildSettings;
+
+        if (currentSceneIndex + 1 < totalScenes)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            Debug.Log("To już ostatni level!");
+            endPanel.SetActive(true);
+        }
     }
 }
